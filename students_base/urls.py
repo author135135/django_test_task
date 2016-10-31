9e5e3fe9b5dde514e0ecf4f583c4363bc09d6230
ram <vitalyturkov@gmail.com>
2016-10-31 03:27:14 +0200

    random change

--- students_base/urls.py
+++ students_base/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('students_base_app.urls', namespace='students_app')),
